c5af9d2f4609e1084bab131b533bc81c189a5b78
Omar HAMZA <omarhamza37@gmail.com>
2025-06-05 18:03:37 +0100

    Add entities

---
+++ TradingNotifications.Domain/Entities/Candle.cs
@@ -0,0 +1,11 @@
+﻿namespace TradingNotifications.Domain.Entities;
+
+public class Candle
+{
+    public DateTime OpenTime { get; set; }
+    public decimal Open { get; set; }
+    public decimal High { get; set; }
+    public decimal Low { get; set; }
+    public decimal Close { get; set; }
+    public decimal Volume { get; set; }
+}
---
+++ TradingNotifications.Domain/Entities/CryptoMonitorSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace TradingNotifications.Domain.Entities;
+
+public class CryptoMonitorSettings
+{
+    public List<string> CryptoList { get; set; }
+    public int SmaPeriod { get; set; }
+    public int RsiPeriod { get; set; }
+    public int RsiBuyThreshold { get; set; }
+}
