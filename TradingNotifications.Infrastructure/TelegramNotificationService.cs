62a76fcd24f8233e8f4811fcf746fe80f272632d
Omar HAMZA <omarhamza37@gmail.com>
2025-06-05 18:53:45 +0100

    Handle telegram notification service

---
+++ TradingNotifications.Infrastructure/TelegramNotificationService.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using TradingNotifications.Application;
+using TradingNotifications.Domain.Entities;
+
+namespace TradingNotifications.Infrastructure;
+
+public class TelegramNotificationService : INotificationService
+{
+    private readonly string _botToken;
+    private readonly string _chatId;
+
+    public TelegramNotificationService(string botToken, string chatId)
+    {
+        _botToken = botToken;
+        _chatId = chatId;
+    }
+
+    public async Task SendNotificationAsync(Notification notification)
+    {
+        var url = $"https://api.telegram.org/bot{_botToken}/sendMessage?chat_id={_chatId}&text={Uri.EscapeDataString(notification.Message)}";
+
+        using var httpClient = new HttpClient();
+        var response = await httpClient.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+    }
+}
