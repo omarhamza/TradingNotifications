75afc592eead3a44492976eae1bd03fb368ce6dd
Omar HAMZA <omarhamza37@gmail.com>
2025-06-16 23:14:40 +0100

    feat: handle notification worker

--- TradingNotifications.Functions/NotificationWorker.cs
+++ TradingNotifications.Functions/NotificationWorker.cs
@@ -1,16 +1,28 @@
-using System;
-using Microsoft.Azure.Functions.Worker;
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using TradingNotifications.Application;
+using TradingNotifications.Domain.Entities;
 
 namespace TradingNotifications.Functions;
 
 public class NotificationWorker
 {
     private readonly ILogger _logger;
+    private readonly HttpClient _httpClient = new HttpClient();
+    private readonly IConfiguration _configuration;
+    private readonly INotificationService _notificationService;
+    private readonly ICryptoAnalysisService _notificationProcessor;
 
-    public NotificationWorker(ILoggerFactory loggerFactory)
+    public NotificationWorker(ILoggerFactory loggerFactory,
+        IConfiguration configuration, 
+        INotificationService notificationService, 
+        ICryptoAnalysisService notificationProcessor)
     {
         _logger = loggerFactory.CreateLogger<NotificationWorker>();
+        _configuration = configuration;
+        _notificationService = notificationService;
+        _notificationProcessor = notificationProcessor;
     }
 
     /// <summary>
@@ -21,7 +33,14 @@ public class NotificationWorker
     public void Run([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer)
     {
         _logger.LogInformation("C# Timer trigger function executed at: {executionTime}", DateTime.UtcNow);
-        
+
+        var settings = _configuration.GetSection("CryptoMonitorSettings").Get<CryptoMonitorSettings>() ?? new CryptoMonitorSettings();
+
+        _notificationProcessor
+            .ProcessNotificationsAsync(settings.CryptoList, settings)
+            .GetAwaiter()
+            .GetResult();
+
         if (myTimer.ScheduleStatus is not null)
         {
             _logger.LogInformation("Next timer schedule at: {nextSchedule}", myTimer.ScheduleStatus.Next);
